1d26dfc3380e029861bdcd0e16012ad3544aa58c
AISW509-205 <dyubleh@gmail.com>
2024-03-21 10:25:57 +0900

    .

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -25,6 +26,19 @@ public class MemoRepositoryTests {
             Memo memo = Memo.builder().memoText("Dummy Data Test" + i).build();
             memoRepository.save(memo);
         });
+    }
 
+    @Test
+    public void testSelect(){
+        Long mno =100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("=============================================");
+
+        if (result.isPresent()){
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
     }
 }
